ec9cc5c18a375078d45bb7acaedf5ef55a416cce
anupkumar-mca-hbtu <123875466+anupkumar-mca-hbtu@users.noreply.github.com>
2024-03-09 23:45:31 +0530

    string

--- src/String/checkForAnagram.java
+++ src/String/checkForAnagram.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 
 public class checkForAnagram {
+
     // Naive approach
     public static boolean isAnagram(String str1,String str2) {
 
@@ -25,13 +26,34 @@ public class checkForAnagram {
        return false;
     }
 
+    // efficient Approach
+    static final int CHAR =256;
+
+    public static boolean isAnagram2(String str1, String str2) {
+       int []a= new int[CHAR];
+       if(str1.length()!=str2.length()) {
+           return false;
+       }
+       for(int i=0;i<str1.length();i++) {
+           a[str1.charAt(i)]++;
+           a[str2.charAt(i)]--;
+       }
+       for(int i=0;i<CHAR;i++) {
+           if(a[i]>0 || a[i]<0) {
+               return false;
+           }
+       }
+       return true;
+    }
+
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in) ;
 
         String s1= sc.next();
         String s2= sc.next();
 
-        boolean flag = isAnagram(s1,s2);
+       // boolean flag = isAnagram(s1,s2);
+        boolean flag = isAnagram2(s1,s2);
         System.out.println(flag);
 
     }
