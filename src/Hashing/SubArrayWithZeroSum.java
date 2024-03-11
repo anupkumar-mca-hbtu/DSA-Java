ea0c110e5c54ee431a05fe615e2a79e8f86c3933
anupkumar-mca-hbtu <123875466+anupkumar-mca-hbtu@users.noreply.github.com>
2024-03-11 17:12:52 +0530

    Hashing

---
+++ src/Hashing/SubArrayWithZeroSum.java
@@ -0,0 +1,26 @@
+package Hashing;
+
+import java.util.HashSet;
+
+public class SubArrayWithZeroSum {
+    public static void main(String[] args) {
+        int[] arr = {5, -4, -3, -1};
+        System.out.print(findsum(arr, arr.length));
+
+    }
+    static boolean findsum(int arr[],int n)
+    {
+        //Your code here
+        int preSum=0;
+        HashSet<Integer> hs= new HashSet<>();
+        for(int i=0;i<n;i++) {
+            preSum=preSum+arr[i];
+            if(preSum==0) {
+                return true;
+            }
+
+            hs.add(arr[i]);
+        }
+        return false;
+    }
+}
